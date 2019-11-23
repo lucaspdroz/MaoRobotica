3efb00684563fbd2f5741710ae4f97c3ae7bf914
Nikolas Freitas <nikolasbvf@gmail.com>
2019-11-23 16:14:12 -0300

    :construction: Add functions and more fingers

--- Arduino/FuncionandoServoIno/FuncionandoServoIno.ino
+++ Arduino/FuncionandoServoIno/FuncionandoServoIno.ino
@@ -2,19 +2,53 @@
 // according to the value
 #include <Servo.h>
 
-Servo myservo;                   // Create servo object to control a servo
-int servoPin = 3;                // Connect yellow servo wire to digital I/O pin 3 (must be PWM)
+Servo thumb, pointer, middle, ring, pinky; // Create servo object to control a servo
+                // Connect yellow servo wire to digital I/O pin 3 (must be PWM)
 int val = 0;                     // Data received from the serial port
 
 void setup() {
-  myservo.attach(servoPin);      // Attach the servo to the PWM pin
+  // Attach fingers
+  thumb.attach(3);
+  pointer.attach(12);
+  middle.attach(4);
+  ring.attach(5);
+  pinky.attach(6);
+  // ======== || ========
+
+  // 
   Serial.begin(9600);           // Start serial communication at 9600 bps
+  
+  //  Reset all position
+  thumb.write(0);
+  pointer.write(0);
+  middle.write(0);
+  ring.write(0);
+  pinky.write(0);
 }
 
 void loop() {
   if (Serial.available()) {      // If data is available to read,
     val = Serial.read();         // read it and store it in val
   }
-  myservo.write(val);            // Set the servo position
-  delay(15);                     // Wait for the servo to get there
+
+  switch(val) {
+    case 'a':
+      thumb.write(180);
+     break;
+
+    case 'b':
+      thumb.write(90);
+      pointer.write(90);
+      break;
+    default:
+      likeHand();
+      break;
+  }
+              // Set the servo position
+  delay(3000);                     // Wait for the servo to get there
+}
+
+void likeHand() {
+  thumb.write(180);
+  pointer.write(0);
 }
